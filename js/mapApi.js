5f25d444224fd09c7460121e5b432a8ae619132e
황 영근 <hollowinday@Neowiz.com>
2020-01-21 17:35:00 +0900

    테스트 코드입니다.

--- js/mapApi.js
+++ js/mapApi.js
@@ -2,7 +2,7 @@ function initialize() {
 
 			/*
 				http://openapi.map.naver.com/api/geocode.php?key=f32441ebcd3cc9de474f8081df1e54e3&encoding=euc-kr&coord=LatLng&query=서울특별시 강남구 강남대로 456
-                위의 링크에서 뒤에 주소를 적으면 x,y 값을 구할수 있습니다.
+				위의 링크에서 뒤에 주소를 적으면 x,y 값을 구할수 있습니다.
 			*/
 			var Y_point			= 37.400286;		// Y 좌표
 			var X_point			= 127.104363;		// X 좌표
@@ -12,9 +12,9 @@ function initialize() {
 
 			// 말풍선 내용
 			var contentString	= '<div>' 
-			                    + '<h2>(주)겜프스</h2>'
-			                    + '<p> 경기 성남시 분당구 삼평동 629 네오위즈 판교타워 <br />' 
-			                    +'</div>';
+								+ '<h2>(주)겜프스</h2>'
+								+ '<p> 경기 성남시 분당구 삼평동 629 네오위즈 판교타워 <br />' 
+								+'</div>';
 
 			var myLatlng = new google.maps.LatLng(Y_point, X_point);
 			var mapOptions = {
@@ -45,8 +45,11 @@ function initialize() {
 		
 
 function testcommitcheck() {
-		console.info("test");
+		
 		/// testcode
+
+		const data = "test"
+		console.info(data);
 	}
 		
 		
